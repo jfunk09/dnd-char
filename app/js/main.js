fe7042ed7958a9ca2c5b188cdd146486276cee8c
James Funk <jfunk09@gmail.com>
2014-11-16 19:08:57 -0800

    set up some states

--- app/js/main.js
+++ app/js/main.js
@@ -14,15 +14,79 @@ var app = angular.module('dndCharApp', [
 	'jqueryModule'
 ]);
 
-angular.module('dndCharApp')
-	.directive('main', [function () {
-		return {
-			restrict: 'E',
-			controller: 'MainCtrl',
-			template: '{{mfVar}}'
-		};
+app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+	var defaultUrl = '/app';
+
+	$locationProvider.html5Mode(true);
+	$locationProvider.hashPrefix('!');
+	$urlRouterProvider.when('', defaultUrl);
+	$urlRouterProvider.otherwise(defaultUrl);
+	$stateProvider
+		.state('root', {
+			url: ''
+		})
+		.state('app', {
+			url: defaultUrl,
+			views: {
+				'mainMenu': {
+					template: '<div>To be a main menu</div>'
+				},
+				'content': {
+					template: '<div>Home page content</div>'
+				}
+			}
+		})
+		.state('app.test', {
+			url: '/test',
+			views: {
+				'content@': {
+					template: '<div>test</div>'
+				}
+			}
+		})
+		.state('app.characterList', {
+			url: '/characters',
+			views: {
+				'content@': {
+					templateUrl: 'partials/characterList.html',
+					controller: 'MainCtrl'
+				}
+			}
+		})
+		.state('app.createCharacter', {
+			url: '/createCharacter',
+			views: {
+				'content@': {
+					templateUrl: 'partials/createCharacter.html',
+					controller: 'AddCtrl'
+				}
+			}
+		});
+}]);
+
+app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+		$http.get('/api/characters')
+			.success(function (data) {
+				console.log(data);
+				$scope.characters = data;
+			})
+			.error(function (data) {
+				console.log('error');
+			});
 	}]);
 
-app.controller('MainCtrl', ['$scope', function ($scope) {
-		$scope.mfVar = 'a scope variable';
+app.controller('AddCtrl', ['$scope', '$http', function ($scope, $http) {
+		$scope.name = null;
+		$scope.race = null;
+		$scope.charClass = null;
+		
+		$scope.addCharacter = function () {
+			$http.post('/api/addCharacter', {name: $scope.name, race: $scope.race, charClass: $scope.charClass})
+				.success(function (data) {
+			
+				})
+				.error(function (data) {
+			
+				});
+		};
 	}]);
--- server.js
+++ server.js
@@ -1,26 +1,90 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var mongo = require('mongodb').MongoClient;
+var Character = require('./lib/character.js');
 
 app.engine('html', require('ejs-locals'));
+app.set('view engine', 'html');
 
 app.use(express.static(__dirname + '/app'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use('/partials', express.static(__dirname + '/views/partials'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
-	res.render('index.html');
-});
+var dbUrl = 'mongodb://localhost:27017/dndChar';
 
 app.get('/api/characters', function (req, res) {
-	mongo.connect('mongodb://localhost:27017/dndChar', function (err, db) {
+	mongo.connect(dbUrl, function (err, db) {
 		if (err) {
 			res.send('error');
 			return;
 		}
-		var characters = db.collection('characters');
-		db.close();
-		return {some: 'data'};
+		var charactersCollection = db.collection('characters');
+		var characters = [];
+		
+		var stream = charactersCollection.find().stream();
+		stream.on('data', function (data) {
+			var character = new Character(data);
+			characters.push(character.toJSON());
+		});
+		stream.on('end', function () {
+			res.send(characters);
+			db.close();
+		});
 	});
+});
+
+app.post('/api/addCharacter', function (req, res) {
+	var character = new Character(req.body);
+	mongo.connect(dbUrl, function (err, db) {
+		if (err) {
+			res.send('error');
+			return;
+		}
+		var cc = db.collection('characters');
+		cc.insert(character.toJSON(), {w:1}, function (err, result) {
+			console.log('Insert Character', result);
+			db.close();
+			res.sendStatus(200);
+		});
+	});
+});
+
+app.get('/api/update', function (req, res) {
+	mongo.connect(dbUrl, function (err, db) {
+		if (err) {
+			res.send('error');
+			return;
+		}
+		res.sendStatus(200);
+		var cc = db.collection('characters');
+		/*cc.insert({name: 'jeffers'}, {w:1}, function (err, result) {
+			console.log('insert result', result);
+			db.close();
+			res.sendStatus(200);
+		});
+		
+		var stream = cc.find({race:'human'}).stream();
+		stream.on('data', function (character) {
+			cc.remove(character, {w:1}, function () {});
+		});
+		stream.on('end', function () {
+			db.close();
+			res.sendStatus(200);
+		});
+		
+		/*cc.update({name:'jeffers'}, {$set:{race: 'human'}}, {w:1}, function (err, result) {
+			console.log('update result', result);
+			db.close();
+			res.sendStatus(200);
+		});*/
+	});
+});
+
+app.get('/*', function (req, res) {
+    res.sendFile('views/index.html', { root: __dirname });
 });
 
 var server = app.listen(9000, function () {
